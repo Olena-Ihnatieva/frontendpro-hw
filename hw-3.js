48714b89dda28c377b2a39f357ca370f28d43d3e
Olena Ihnatieva <ihnatievaolena@gmail.com>
2024-01-19 19:03:22 +0200

    hw-3 done

---
+++ hw-3.js
@@ -0,0 +1,11 @@
+let number_first = +prompt('Enter the first number');
+
+let number_second = +prompt('Enter the second number');
+
+let number_third = +prompt('Enter the third number');
+
+let result = (number_first + number_second + number_third) / 3;
+
+alert(result);
+
+
