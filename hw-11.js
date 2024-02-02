4317c2eacde769d5f85161fc129be758ca0d82cb
Olena Ihnatieva <ihnatievaolena@gmail.com>
2024-02-02 21:03:13 +0200

    hw-11 in progress

---
+++ hw-11.js
@@ -0,0 +1,13 @@
+/*const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+function generateKey(length, characters) {
+    let min = 0;
+    let max = characters.length;
+
+    let result = Math.floor(
+        Math.random() * (max - min + 1) + min
+    );
+    return result;
+};
+
+const key = generateKey(16, characters);
+console.log(key);*/
