97c0285cae1a156428866d94fd999fae3dc16899
Olena Ihnatieva <ihnatievaolena@gmail.com>
2024-02-02 21:09:22 +0200

    hw-12 done

---
+++ hw-12.js
@@ -0,0 +1,13 @@
+/*10 * 10 * 10 = 1000
+10 * 10 = 100
+10 * 1 = 10 //цикл окончен*/
+function pow(num, degree) {
+    if (degree > 0) {
+        return num * pow(num, degree - 1);
+    } else {
+        return 1;
+    }
+}
+
+const result = pow(10, 3);
+console.log(result);
