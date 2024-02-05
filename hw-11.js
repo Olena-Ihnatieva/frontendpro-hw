c487d1ec96795769436ee2d2a78ffc1c27ca848c
Olena Ihnatieva <ihnatievaolena@gmail.com>
2024-02-05 13:27:55 +0200

    hw-11 done

--- hw-11.js
+++ hw-11.js
@@ -1,13 +1,17 @@
-/*const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
 function generateKey(length, characters) {
     let min = 0;
     let max = characters.length;
+    let result = 0;         //let result = ''; ?
 
-    let result = Math.floor(
-        Math.random() * (max - min + 1) + min
-    );
+    for (let i = 0; i < length; i++) {
+        let charactersR = Math.floor(
+            Math.random() * (max - min + 1) + min
+        );
+        result += characters[charactersR];
+    }
     return result;
-};
+}
 
 const key = generateKey(16, characters);
-console.log(key);*/
+console.log(key);
